d040bd161d27785764ab0e1d48cbbf663cc86de5
erpyjune <thanks.lot@gmail.com>
2014-07-08 14:54:05 +0900

    modified:   okoutdoor.php 	deleted:    okoutdoor.sh

--- bin/okoutdoor.php
+++ bin/okoutdoor.php
@@ -1,219 +1,68 @@
 <?php
 
-include "../lib/curl.class.php";
-include "../lib/parser.class.php";
-include "../lib/db.class.php";
+include "../lib/okoutdoor.class.php";
 
-class OClick {
-
-	var $bestDealUrl = 'http://www.okoutdoor.com/product/product.html?p_category_id=B&page=';
-
-	var $agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)';
-
-	var $start_pos = '';
-	var $end_pos = '';
-
-	var $title_s = '<span class="prName_PrName">';
-	var $title_e = '</span>';
-
-	var $list_s = '<p class="item_title" name="shortProductName">';
-	var $list_e = '<div class="val_top';
-	//var $list_e = '<div class="gift_wrap">';
-
-	var $brand1_s = '<span class="prName_Brand">';
-	var $brand1_e = '</span>';
-
-	var $brand2_s = '<p class="brand_name">브랜드명:';
-	var $brand2_e = '</p>';
-
-	var $cate1_s = '<div class="AttrIcon5">';
-	var $cate1_e = '</div>';
-
-	var $cate2_s = '<dd>';
-	var $cate2_e = '</dd>';
-
-	var $link_s = 'href="';
-	var $link_e = '"';
-
-	var $thumb_s     = '<!-- 상품이미지 -->';
-	var $thumb_sub_s = "data-original='";
-	var $thumb_sub_e = "'";
-	var $thumb_e     = '</a>';
-
-	var $sale_per_s = '<p class="icon">';
-	var $sale_per_e = '%</p>';
-
-	var $org_price_s = '<span class="l">정찰 판매가</span>';
-	var $org_price_sub_s = '<span class="r ">';
-	var $org_price_sub_e = '원</span>';
-	var $org_price_e = '</div>';
-
-	var $sale_price_s = '할인가 : </label><strong>';
-	var $sale_price_sub_s = '<span class="r ">';
-	var $sale_price_sub_e = '원<a name="viewPrice"';
-	var $sale_price_e = '</strong>';
-
-	var $low_price_s = '<span class="l">국내 최저가</span>';
-	var $low_price_sub_s = '<span class="r ">';
-	var $low_price_sub_e = '원<a name="viewPrice"';
-	var $low_price_e = '</span>';
-
-	var $special_price_s = '<span class="l">우수회원 최대 할인가</span>';
-	var $special_price_sub_s = '<span class="r ">';
-	var $special_price_sub_e = '원<a name="viewPrice"';
-	var $special_price_e = '</div>';
-
-	///////////////////////////////////////////////////////////
-	public function __construct() {
-		;
-   }
-
-	///////////////////////////////////////////////////////////
-   public function __destruct() {
-		;
-   }
-
-} // class
-
-
+$seed_url_arr = array(
+						"http://www.okoutdoor.com/product/product.html?p_category_id=T&page=",
+						"http://www.okoutdoor.com/product/product.html?p_category_id=B&page=",
+						"http://www.okoutdoor.com/product/product.html?p_category_id=D&page=",
+						
+/*
+COOLER="http://www.okoutdoor.com/product/product.html?p_category_id=T&page="
+SHOES="http://www.okoutdoor.com/product/product.html?p_category_id=B&page="
+BAG="http://www.okoutdoor.com/product/product.html?p_category_id=D&page="
+SLEEPING="http://www.okoutdoor.com/product/product.html?p_category_id=U&page="
+TENT="http://www.okoutdoor.com/product/product.html?p_category_id=F&page="
+MAT="http://www.okoutdoor.com/product/product.html?p_category_id=V&page="
+LANTERN="http://www.okoutdoor.com/product/product.html?p_category_id=E&page="
+STOVE="http://www.okoutdoor.com/product/product.html?p_category_id=W&page="
+COPEL="http://www.okoutdoor.com/product/product.html?p_category_id=Z&page="
+KNIFE="http://www.okoutdoor.com/product/product.html?p_category_id=I&page="
+WATCH="http://www.okoutdoor.com/product/product.html?p_category_id=K&page="
+GLASS="http://www.okoutdoor.com/product/product.html?p_category_id=L&page="
+EAT="http://www.okoutdoor.com/product/product.html?p_category_id=7&page="
+*/
 
 ///////////////////////////////////////////////////////////
-if ($argc < 2) {
-	die("needs page num!!\n");
+//
+if ($argc < 3) {
+	die("(usage) page_num request_url\n");
 }
 
-$sQuery = urlencode($argv[1]);
 mb_internal_encoding("UTF-8");
 
-$cp = new OClick;
+$cp = new OKOutdoor;
 $cl = new EPCurl;
 $pa = new EPParser;
 $db = new EPDB;
 
-// searching...
-$getUrl = $cp->bestDealUrl . $sQuery;
-$r = $cl->requestGetDataFromUrl($getUrl);
+///////////////////////////////////////////////////////////
+// make request url.
+$page_num = $argv[1];
+$req_url  = $argv[2];
+$url      = $req_url . $page_num;
+
+///////////////////////////////////////////////////////////
+// 검색결과 요청.
+$r = $cl->requestGetDataFromUrl($url);
+
+///////////////////////////////////////////////////////////
+// 수집한 검색결과에서 리스트별로 추출하여 array에 담는다.
 $body = iconv("EUC-KR", "UTF-8", $r);
 $search_list = $pa->getList($body, $cp->list_s, $cp->list_e);
-
 //print_r($search_list);
 
-////////////////////////////////////////////
-// db connect
-$db->connect();
+///////////////////////////////////////////////////////////
+// 검색결과 list에서 item 추출하여 array에 담당 리턴.
+$result_item = $cp->parsePrdtInfo($search_list, $cp, $pa);
+//print_r($result_item);
 
-$total_proc_count = 0;
-$total_insert_count = 0;
-$total_skip_count = 0;
+///////////////////////////////////////////////////////////
+// 검색결과 list에서 item 추출하여 array에 담당 리턴.
+$cp->putPrdtInfoToDB($result_item, $cp, $db);
 
-foreach ($search_list as $list) {
-	//echo "================================\n";
-	$result = $pa->getItem($list, $cp->title_s, $cp->title_e);
-	$t_title = trim($result);
-	echo "title --> $t_title\n";
-
-	$result = $pa->getItem($list, $cp->brand1_s, $cp->brand1_e);
-	$t_brand1 = trim($result);
-	//echo "brand1 --> $t_brand1\n";
-
-	$result = $pa->getItem($list, $cp->brand2_s, $cp->brand2_e);
-	$t_brand2 = trim($result);
-	//echo "brand2 --> $t_brand2\n";
-	{
-		$t_brand_str = "";
-		if (strcmp($t_brand1, "START_POS_NOT")!=0) {
-			$t_brand_str = $t_brand1;
-		}
-
-		if (strcmp($t_brand2, "START_POS_NOT")!=0) {
-			$t_brand_str = $t_brand_str . " " . $t_brand2;
-		}
-
-		echo "brand --> $t_brand_str\n";
-	}
-
-	$result = $pa->getItem($list, $cp->cate2_s, $cp->cate2_e);
-	$t_cate2 = trim($result);
-	if (strcmp($t_cate2, "START_POS_NOT") == 0) {
-		$t_cate2 = "";
-	}
-	echo "cate2 --> $t_cate2\n";
-
-	$result  = $pa->getItem($list, $cp->link_s, $cp->link_e);
-	$t_link = trim($result);
-	echo "link --> $t_link\n";
-
-	$r      = $pa->getItem($list, $cp->thumb_s, $cp->thumb_e);
-	$result = $pa->getItem($r, $cp->thumb_sub_s, $cp->thumb_sub_e);
-	$t_thumb = trim($result);
-	echo "thumb --> $t_thumb\n";
-
-	$r = $pa->getItem($list, $cp->sale_per_s, $cp->sale_per_e);
-	$result = str_replace(",","",$r);
-	$t_sale_per = trim($result);
-	echo "sale_per --> $t_sale_per\n";
-
-	$r  = $pa->getItem($list, $cp->org_price_s, $cp->org_price_e);
-	$rr = $pa->getItem($r, $cp->org_price_sub_s, $cp->org_price_sub_e);
-	$result = str_replace(",","",$rr);
-	$t_org_price = trim($result);
-	echo "org_price --> $t_org_price\n";
-
-	$r  = $pa->getItem($list, $cp->sale_price_s, $cp->sale_price_e);
-	$rr = $pa->getItem($r, $cp->sale_price_sub_s, $cp->sale_price_sub_e);
-	$result = str_replace(",","",$rr);
-	$t_sale_price = trim($result);
-	//echo "sale_price --> $t_sale_price\n";
-
-	$r  = $pa->getItem($list, $cp->low_price_s, $cp->low_price_e);
-	$rr = $pa->getItem($r, $cp->low_price_sub_s, $cp->low_price_sub_e);
-	$result = str_replace(",","",$rr);
-	$t_low_price = trim($result);
-	//echo "low_price --> $t_low_price\n";
-
-	$sale_price_str = "";
-	{
-		if (strcmp($t_sale_price, "START_POS_NOT")==0) {
-			$sale_price_str = $t_low_price;
-		}
-
-		if (strcmp($t_low_price, "START_POS_NOT")==0) {
-			$sale_price_str = $t_sale_price;
-		}
-	}
-	echo "sale_price --> $sale_price_str\n";
-
-	$r  = $pa->getItem($list, $cp->special_price_s, $cp->special_price_e);
-	$rr = $pa->getItem($r, $cp->special_price_sub_s, $cp->special_price_sub_e);
-	$result = str_replace(",","",$rr);
-	$t_special_price = trim($result);
-	echo "special_price --> $t_special_price\n";
-
-	$t_sell_count = 0;
-
-   $s_sql = "select link from social_shop_t where link = '$t_link'";
-   if ($db->data_exist($s_sql) == 0) {
-      $t_sql = "INSERT INTO SOCIAL_SHOP_T (title, cmt, brand, cate, link, thumb, price_org, price_sale, price_special, sale_per, sell_count, cp)
-         VALUES ('$t_title', '', '$t_brand_str', '$t_cate2', '$t_link', '$t_thumb', '$t_org_price', '$sale_price_str', '$t_special_price', '$t_sale_per', $t_sell_count, 'ok')";
-      $db->select($t_sql);
-      echo "(INSERT) $t_title\n";
-      $total_insert_count++;
-   }
-   else {
-      echo "#SKIP# $t_title\n";
-      $total_skip_count++;
-   }
-
-	$total_proc_count++;
-	echo "================================\n";
-}
-
-$db->commit();
-$db->close();
-
-echo "total proc   count --> " . $total_proc_count . "\n";
-echo "total insert count --> " . $total_insert_count . "\n";
-echo "total skip   count --> " . $total_skip_count . "\n";
-echo "call url --> $getUrl\n";
+echo "total process count --> $cp->total_process_count\n";
+echo "total insert  count --> $cp->total_insert_count\n";
+echo "total skip    count --> $cp->total_skip_count\n";
 
 ?>
